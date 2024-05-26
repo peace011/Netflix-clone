e1b1c242791847c41e76c2f3f384192cb274c98b
peace011 <gpuja7044@gmail.com>
2024-05-26 21:14:24 +0545

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 
 const Header = () => {
+  const [background,setBackground]=useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll=()=>{
+      if(window.scrollY>100){
+        setBackground(true);
+      }else{
+        setBackground(false);
+      }
+    };
+    window.addEventListener("scroll",handleScroll);
+    return()=>{
+      window.removeEventListener("scroll",handleScroll);
+    }
+  })
+
   return (
-    <div className='nav'>
+    <div className={`nav ${background? "nav_background": '' }`}>
         <img className='nav_logo' 
         src='https://1000logos.net/wp-content/uploads/2017/05/Netflix-symbol.jpg'/>
         <img className='nav_avatar'
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState } from 'react'
 import axios from '../axios'
 import './Row.css'
 import { useQuery } from 'react-query';
+import Youtube from 'react-youtube'
+// import movieTrailer from 'movie-trailer'
 
 interface RowPropsType{
     title:string,
@@ -17,6 +19,8 @@ const Row= ({title,fetchUrl,isLargeRow}:RowPropsType) => {
 
     const base_Url="https://image.tmdb.org/t/p/original";
 
+    // const [trailerUrl,setTrailerUrl]=useState<string>('')
+
     const fetchData=async()=>{
                 const res = await axios.get(fetchUrl);
                 return res.data.results;
@@ -31,19 +35,43 @@ const Row= ({title,fetchUrl,isLargeRow}:RowPropsType) => {
       return <p>Error</p>
     }
       
-
+    // const opts={
+    //   height:'390',
+    //   width:'100%',
+    //   playerVars:{
+    //     autoplay:1,
+    //   }
+    // }
+    // const handleClick = (movie) => {
+    //   if (trailerUrl) {
+    //     setTrailerUrl('')
+    //   } else {
+    //     movieTrailer(movie?.name || '')
+    //       .then(url => {
+    //         const urlParams = new URLSearchParams(new URL(url).search)
+  
+    //         setTrailerUrl(urlParams.get('v'))
+    //       })
+    //       .catch(error => console.error(error.message))
+    //   }
+    // }
+    
+    
   return (
     <div className='row'>
-        <h2>{title}</h2>
+        <h2 className='row_title'>{title}</h2>
         <div className="row_posters">
             {data && data.map((movie:movieType)=>(
                 <div key={movie.id} className='boxposter'>
                     <img src={`${base_Url}${isLargeRow? movie.poster_path: movie.backdrop_path}`} 
-                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}/>
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    // onClick={()=>handleClick(movie)}
+                    />
                     </div>
             ))}
         </div>
-        
+        {/* {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>} */}
+
     </div>
   )
 }
